0ad6ea8029567234586e5e2a875e1a63c93c9b64
hyun <hyun87@mz.co.kr>
2022-09-14 16:40:38 +0900

    Make Project

---
+++ src/main/java/hyun/meetingmemo/MeetingMemoApplication.java
@@ -0,0 +1,13 @@
+package hyun.meetingmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyun/meetingmemo/MeetingMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun.meetingmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
